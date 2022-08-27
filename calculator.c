5cd44948271c4b0d8aed6475f41c70c680cef65b
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-27 21:39:36 +0300

    fix(calculator): exit code through calculating functions

--- calculator.c
+++ calculator.c
@@ -11,9 +11,9 @@ int calculate(char* expression, double *result) {
     int ex_code = 0;
 
     node *output_stack = init_node();
-    validate_input(expression);
-    parse_input(expression, output_stack);
-    *result = evaluate(output_stack);
+    ex_code = validate_input(expression);
+    if (ex_code == 0) ex_code = parse_input(expression, output_stack);
+    if (ex_code == 0) *result = evaluate(output_stack);
     clean(output_stack);
 
     return ex_code;
