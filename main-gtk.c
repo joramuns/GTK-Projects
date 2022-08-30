bd4f15a8efb35b16a2e09e9727bcb43ecd136692
Joramun Sasin <joramuns@student.21-school.ru>
2022-08-30 17:37:29 +0300

    fix(gtk): no segfault on DEL button

--- main-gtk.c
+++ main-gtk.c
@@ -19,10 +19,11 @@ void add_text(GtkButton *widget, gpointer data) {
     buttonData *current = data;
     GtkText *entry = current->entry;
     GtkEntryBuffer *entryBuffer = gtk_text_get_buffer(entry);
-    char *output = (char *) malloc(1 + strlen(gtk_entry_buffer_get_text(entryBuffer)) + strlen(current->value));
+    size_t length_buf = strlen(gtk_entry_buffer_get_text(entryBuffer));
+    char *output = (char *) malloc(1 + length_buf + strlen(current->value));
     strcpy(output, gtk_entry_buffer_get_text(entryBuffer));
     strcat(output, current->value);
-    gtk_entry_buffer_set_text(entryBuffer, output, (int) strlen(output));
+    gtk_entry_buffer_set_text(entryBuffer, output, strlen(output));
     free(output);
 }
 
@@ -30,13 +31,14 @@ void del_text(GtkButton *widget, gpointer data) {
     buttonData *current = data;
     GtkText *entry = current->entry;
     GtkEntryBuffer *entryBuffer = gtk_text_get_buffer(entry);
-    int length = (int) strlen(gtk_entry_buffer_get_text(entryBuffer));
-    if (length > 0) {
-        char output[--length];
-        for (int i = 0; i < length; ++i)
-            output[i] = gtk_entry_buffer_get_text(entryBuffer)[i];
-        gtk_entry_buffer_set_text(entryBuffer, output, length);
+    size_t length_buf = strlen(gtk_entry_buffer_get_text(entryBuffer));
+    char *output = (char *) malloc(1 + length_buf);
+    if (length_buf > 0) {
+        strcpy(output, gtk_entry_buffer_get_text(entryBuffer));
+        output[length_buf - 1] = '\0';
+        gtk_entry_buffer_set_text(entryBuffer, output, strlen(output));
     }
+    free(output);
 }
 
 void get_result(GtkButton *widget, gpointer data) {
@@ -64,46 +66,20 @@ static void activate (GtkApplication *app, gpointer user_data) {
     GObject *window = gtk_builder_get_object(builder, "window");
     gtk_window_set_application(GTK_WINDOW (window), app);
 
-//    buttonex = gtk_builder_get_object (builder, "quit");
-//      g_signal_connect_swapped (buttonex, "clicked", G_CALLBACK (quit_cb), window);
+    /* style */
+    GtkCssProvider *cssProvider = gtk_css_provider_new();
+    GdkDisplay *display = gtk_widget_get_display(GTK_WIDGET(window));
+//    gtk_css_provider_load_from_data(cssProvider, "text {background-color: white; font-family: monospace; font-size: 18pt;}", -1);
+    gtk_css_provider_load_from_path(cssProvider, "theme.css");
+    gtk_style_context_add_provider_for_display(display, GTK_STYLE_PROVIDER(cssProvider), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION);
+    g_object_unref(cssProvider);
 
-
+    /* interface */
     buttonData *buttons = user_data;
 
     char *names[] = {"button0", "button1", "button2", "button3", "button4", "button5", "button6", "button7", "button8", "button9", "buttondot", "buttonsum", "buttonsub", "buttonmul", "buttondiv", "quit", "buttondel", "buttonres", "buttonzero", "buttonpow", "buttonopenbrace", "buttonclosebrace", "buttonacos", "buttonasin", "buttonatan", "buttonsqrt", "buttonsin", "buttoncos", "buttonmod", "buttontan", "buttonlog", "buttonln"};
     char *values[] = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9", ".", "+", "-", "*", "/", NULL, NULL, NULL, NULL, "^", "(", ")", "acos(", "asin(", "atan(", "sqrt(", "sin(", "cos(", "mod", "tan(", "log(", "ln("};
-//
 
-//    buttons[0].name = names[0];
-//    buttons[0].value = values[0];
-//    buttons[0].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[0].button = (GtkButton *) gtk_builder_get_object(builder, names[0]);
-//    g_signal_connect_swapped (buttons[0].button, "clicked", G_CALLBACK (quit_cb), window);
-//    buttons[1].name = names[1];
-//    buttons[1].value = values[1];
-//    buttons[1].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[1].button = (GtkButton *) gtk_builder_get_object(builder, names[1]);
-//    g_signal_connect_swapped (buttons[1].button, "clicked", G_CALLBACK (quit_cb), window);
-//    buttons[2].name = names[2];
-//    buttons[2].value = values[2];
-//    buttons[2].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[2].button = (GtkButton *) gtk_builder_get_object(builder, names[2]);
-//    g_signal_connect_swapped (buttons[2].button, "clicked", G_CALLBACK (quit_cb), window);
-//    buttons[3].name = names[3];
-//    buttons[3].value = values[3];
-//    buttons[3].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[3].button = (GtkButton *) gtk_builder_get_object(builder, names[3]);
-//    g_signal_connect_swapped (buttons[3].button, "clicked", G_CALLBACK (quit_cb), window);
-//    buttons[5].name = names[5];
-//    buttons[5].value = values[5];
-//    buttons[5].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[5].button = (GtkButton *) gtk_builder_get_object(builder, names[5]);
-//    g_signal_connect_swapped (buttons[5].button, "clicked", G_CALLBACK (quit_cb), window);
-//    buttons[15].name = names[15];
-//    buttons[15].value = values[15];
-//    buttons[15].entry = (GtkText *) gtk_builder_get_object(builder, "entry");
-//    buttons[15].button = (GtkButton *) gtk_builder_get_object(builder, "quit");
-//    g_signal_connect_swapped (buttons[15].button, "clicked", G_CALLBACK (quit_cb), window);
     for (int i = 0; i < 30; ++i) {
             buttons[i].name = names[i];
             buttons[i].value = values[i];
