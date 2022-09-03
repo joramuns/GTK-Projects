6d36916e55eaf7907ee07388c835ae74f79550bc
Joramun Sasin <joramuns@student.21-school.ru>
2022-09-03 14:17:52 +0300

    chore(xcode)

--- main.c
+++ main.c
@@ -8,18 +8,26 @@
 #include <stdio.h>
 #include <string.h>
 #include "calculator.h"
+#include "credit_calculator.h"
 //Unary minus and plus
 //Bracers handling - log(.. has no error
 //Exit codes to finish
 int main() {
     int ex_code = 0;
-    char expression[EXPRESSION_SIZE + 1 + 1] = {0};
-    fgets(expression, EXPRESSION_SIZE + 1 + 1, stdin);
-    double result = 0;
+//    char expression[EXPRESSION_SIZE + 1 + 1] = {0};
+//    fgets(expression, EXPRESSION_SIZE + 1 + 1, stdin);
+//    double result = 0;
+//
+//    ex_code = calculate(expression, &result);
+    credit_input test_case = {0};
+    test_case.sum = 30000;
+    test_case.term = 6;
+    test_case.rate = 0.24;
+    test_case.type = 1;
 
-    ex_code = calculate(expression, &result);
+    eval_differentiated_credit(test_case);
 
-    printf("Answer: %lf\n", result);
+//    printf("Answer: %lf\n", test_case.sum);
 
     return ex_code;
 }
