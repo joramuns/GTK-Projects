fb5fa4b93edc84f9888a619da6d9a739db0f2862
Joramun Sasin <joramuns@student.21-school.ru>
2022-09-08 20:23:03 +0300

    feat(deposit): add withdrawals and replenishments lists

--- main.c
+++ main.c
@@ -5,13 +5,16 @@
 //  Created by Joramun Sasin on 8/25/22.
 //
 
+#include <math.h>
 #include <stdio.h>
 #include <string.h>
 #include "Calculation/calculator.h"
 #include "Calculation/deposit_calculator.h"
-//Unary minus and plus
-//Bracers handling - log(.. has no error
-//Exit codes to finish
+
+double bank_rounding(double value) {
+    return roundf(value * 100) / 100;
+}
+
 int main() {
     int ex_code = 0;
 //    char expression[EXPRESSION_SIZE + 1 + 1] = {0};
@@ -23,13 +26,22 @@ int main() {
     cont.deposit = 300000;
     cont.rate = 0.24;
     cont.term = 179;
-    cont.freq_payment = MONTHLY;
-    cont.capitalization = 1;
+    cont.freq_payment = DAILY;
+    cont.capitalization = 0;
+    int account_movement[cont.term];
+    for (int i = 0; i < cont.term; i++) account_movement[i] = 0;
+
+    /* Careful! Number of day and number of array! */
+    account_movement[14] = -15000;
+    account_movement[29] = 78787;
+    account_movement[99] = -3000;
 
     double temp_deposit = cont.deposit;
     for (unsigned term_count = 1; term_count <= cont.term; term_count++) {
+        cont.deposit += account_movement[term_count - 1];
+        temp_deposit += account_movement[term_count - 1];
         cont.daily_income = (cont.deposit * cont.rate) / YEARDAYS;
-        cont.payout += cont.daily_income;
+        cont.payout += bank_rounding(cont.daily_income);
         if ((term_count % cont.freq_payment) == 0) {
             if (cont.capitalization) {
                 cont.deposit += cont.payout;
