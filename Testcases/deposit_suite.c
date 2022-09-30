7bd61094f41359a911213b44582f59449c874895
Joramun Sasin <joramuns@student.21-school.ru>
2022-09-30 19:53:15 +0300

    tests: fixed deposit test with no leaks

--- Testcases/deposit_suite.c
+++ Testcases/deposit_suite.c
@@ -3,17 +3,17 @@
 START_TEST(s1) {
     deposit_input test_case = {0};
     deposit_output test_result = {0};
-    test_case.deposit = 10000;
-    test_case.term = 180;
-    test_case.rate = 7;
+    test_case.deposit = 10000.0;
+    test_case.term = 181.0;
+    test_case.rate = 7.0;
     test_case.freq_payment = 30;
-    test_case.account_movement = malloc(sizeof(double) * 1);
-    /* for (int i = 0; i < test_case.term; i++) test_case.account_movement[i] = 0; */
-    /* handle_deposit_calc(test_case, &test_result); */
+    test_case.account_movement = malloc(sizeof(double) * test_case.term);
+    for (int i = 0; i < test_case.term; i++) test_case.account_movement[i] = 0;
+    handle_deposit_calc(test_case, &test_result);
+    clean(test_result.stack_of_payouts);
+    free(test_case.account_movement);
     double result_deposit = test_result.total_profit;
     ck_assert_double_eq_tol(result_deposit, 347.11, 1e-02); 
-    /* clean(test_result.stack_of_payouts); */
-    free(test_case.account_movement);
 }
 END_TEST
 
