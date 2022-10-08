faad4d8f781fb42d17e23ae707c0a0c8751590b4
fedor <fedorsah@gmail.com>
2022-10-08 12:42:49 +0300

    Refactored obj_parser tests

--- tests/obj_parser_suite.c
+++ tests/obj_parser_suite.c
@@ -5,10 +5,10 @@
 #define TEST_EPS 1e-6
 
 
-GArray * sc_vertices = NULL;
-GList * sc_faces = NULL;
+static GArray * sc_vertices = NULL;
+static GList * sc_faces = NULL;
 
-void simple_cube_setup(void)
+static void simple_cube_setup(void)
 {
   sc_vertices = g_array_sized_new(FALSE, TRUE, sizeof(GLfloat), 1000);
   ck_assert_int_eq(parse_obj_file("../../misc/obj_files/cube_simple.obj", sc_vertices, &sc_faces), 0); 
@@ -16,7 +16,7 @@ void simple_cube_setup(void)
   ck_assert_ptr_nonnull(sc_vertices);
 }
 
-void simple_cube_teardown(void)
+static void simple_cube_teardown(void)
 {
   g_array_unref(sc_vertices);
   g_list_free_full(sc_faces, (GDestroyNotify) g_array_unref);
@@ -59,18 +59,23 @@ START_TEST(test_simple_cube_last_face) {
   ck_assert_uint_eq(data[9], 2);
 } END_TEST
 
+static TCase * simple_cube_tc(void)
+{
+  TCase *tc = tcase_create("simple_cube");
+  tcase_add_checked_fixture(tc, simple_cube_setup, simple_cube_teardown);
+  tcase_add_test(tc, test_simple_cube_first_vertex);
+  tcase_add_test(tc, test_simple_cube_last_vertex);
+  tcase_add_test(tc, test_simple_cube_first_face);
+  tcase_add_test(tc, test_simple_cube_last_face);
+
+  return tc;
+}
+
 Suite * obj_parser_suite()
 {
   Suite *s = suite_create ("obj_parser_suite");
 
-  TCase *simple_cube_tc = tcase_create("simple_cube");
-  tcase_add_unchecked_fixture(simple_cube_tc, simple_cube_setup, simple_cube_teardown);
-  tcase_add_test(simple_cube_tc, test_simple_cube_first_vertex);
-  tcase_add_test(simple_cube_tc, test_simple_cube_last_vertex);
-  tcase_add_test(simple_cube_tc, test_simple_cube_first_face);
-  tcase_add_test(simple_cube_tc, test_simple_cube_last_face);
-
-  suite_add_tcase(s, simple_cube_tc);
+  suite_add_tcase(s, simple_cube_tc());
 
   return s;
 }
