c7a3877847644e80a2c2b4af55986346fe57ceed
Joramun Sasin <joramuns@student.21-school.ru>
2022-11-05 21:36:11 +0300

    style(header): delete redefined in affine header enum from window header

--- gui/window.h
+++ gui/window.h
@@ -5,19 +5,8 @@
 #define HEIGHT 500
 
 #include "app.h"
+#include "../core/affine_transformations.h"
 #include <gtk/gtk.h>
-
-enum {
-  X_AXIS,
-  Y_AXIS,
-  Z_AXIS,
-  X_MOVE,
-  Y_MOVE,
-  Z_MOVE,
-  SCALE,
-
-  N_AXIS
-};
 
 #define VVIEWER_APP_WINDOW_TYPE (vviewer_app_window_get_type ())
 G_DECLARE_FINAL_TYPE (VviewerAppWindow, vviewer_app_window, VVIEWER, APP_WINDOW, GtkApplicationWindow)
