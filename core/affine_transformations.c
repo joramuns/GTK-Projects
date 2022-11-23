106e9e8b0bca2748cefee1f2d3f27997a3026187
Joramun Sasin <joramuns@student.21-school.ru>
2022-11-23 20:05:03 +0300

    feat(dox): add doxygen description to affine transformations file

--- core/affine_transformations.c
+++ core/affine_transformations.c
@@ -1,5 +1,14 @@
 #include "affine_transformations.h"
 
+/**
+ * @brief Creates matrix that transforms model to perspective projection
+ *
+ * @param dest flat matrix with result transformation
+ * @param near near clipping plane
+ * @param far far clipping plane
+ * @param right side clipping planes
+ * @param top top and bottom clipping planes
+ */
 void perspectiveProjection(float *dest, float near, float far, float right, float top) {
   dest[0] = 1 / right;
   dest[5] = 1 / top;
@@ -8,6 +17,12 @@ void perspectiveProjection(float *dest, float near, float far, float right, floa
   dest[15] = 1;
 }
 
+/**
+ * @brief Fills matrix that rotates model by X axis
+ *
+ * @param res flat matrix with result transformation
+ * @param rotation_angles array with data from axes
+ */
 void fillXaxis(float *res, float *rotation_angles) {
   float x = rotation_angles[X_AXIS] * (M_PI / 180.f);
   float cos = cosf(x), sin = sinf(x);
@@ -17,6 +32,12 @@ void fillXaxis(float *res, float *rotation_angles) {
   res[10] = cos;
 }
 
+/**
+ * @brief Fills matrix that rotates model by Y axis
+ *
+ * @param res flat matrix with result transformation
+ * @param rotation_angles array with data from axes
+ */
 void fillYaxis(float *res, float *rotation_angles) {
   float y = rotation_angles[Y_AXIS] * (M_PI / 180.f);
   float cos = cosf(y), sin = sinf(y);
@@ -26,6 +47,12 @@ void fillYaxis(float *res, float *rotation_angles) {
   res[10] = cos;
 }
 
+/**
+ * @brief Fills matrix that rotates model by Z axis
+ *
+ * @param res flat matrix with result transformation
+ * @param rotation_angles array with data from axes
+ */
 void fillZaxis(float *res, float *rotation_angles) {
   float z = rotation_angles[Z_AXIS] * (M_PI / 180.f);
   float cos = cosf(z), sin = sinf(z);
@@ -35,18 +62,35 @@ void fillZaxis(float *res, float *rotation_angles) {
   res[5] = cos;
 }
 
+/**
+ * @brief Fills matrix that moves model
+ *
+ * @param res flat matrix with result transformation
+ * @param rotation_angles array with data from axes
+ */
 void fillMove(float *res, float *rotation_angles) {
   res[12] = rotation_angles[X_MOVE];
   res[13] = rotation_angles[Y_MOVE];
   res[14] = rotation_angles[Z_MOVE];
 }
 
+/**
+ * @brief Fills matrix that scales model
+ *
+ * @param res flat matrix with result transformation
+ * @param rotation_angles array with data from axes
+ */
 void fillScale(float *res, float *rotation_angles) {
   res[0] = rotation_angles[SCALE];
   res[5] = rotation_angles[SCALE];
   res[10] = rotation_angles[SCALE];
 }
 
+/**
+ * @brief Fills identity matrix
+ *
+ * @param res flat matrix with result transformations
+ */
 void fillIdentityMatrix(float *res) {
   res[0] = 1.0;
   res[5] = 1.0;
